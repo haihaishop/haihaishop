9eca7d10eaf79c972a3f6410d38aa2546afffbd4
小朝 <13942156310@163.com>
2017-07-18 15:14:51 +0800

    a

--- src/main/java/com/shop/controller/admin/SuperAdmin.java
+++ src/main/java/com/shop/controller/admin/SuperAdmin.java
@@ -7,6 +7,7 @@ import com.shop.model.service.RoleManagerInterface;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
