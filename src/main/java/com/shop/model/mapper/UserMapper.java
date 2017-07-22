846899e9dce2ab171407c921787e20d50d1b0b28
wills <18240440420@163.com>
2017-07-22 16:57:28 +0800

    change information

--- src/main/java/com/shop/model/mapper/UserMapper.java
+++ src/main/java/com/shop/model/mapper/UserMapper.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.*;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+
 import org.apache.ibatis.annotations.Select;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -28,21 +29,28 @@ public interface UserMapper {
     User getUserByLoginName(@Param("username") String username);
 
     @Select("select * from user where role_id=#{role_id}")
-    List<User> getUsersByRoleId(@Param("role_id")Long role_id);
+    List<User> getUsersByRoleId(@Param("role_id") Long role_id);
 
     @Delete("delete from user where username=#{username}")
-    void deleteUserByLoginName(@Param("username")String username);
+    void deleteUserByLoginName(@Param("username") String username);
 
     @Select("select * from user where username=#{username}")
-    User loginUser(@Param("username")String username);
+    User loginUser(@Param("username") String username);
 
     @Update("update user set password=#{password} where username=#{username}")
-    void changePasswordByUsername(@Param("username")String username,
-                                  @Param("password")String password);
+    void changePasswordByUsername(@Param("username") String username,
+                                  @Param("password") String password);
 
     @Select("select password from user where username=#{username}")
-    String getPasswordByUsername(@Param("username")String username);
+    String getPasswordByUsername(@Param("username") String username);
 
     @Select("select role_id from user where username = #{username}")
-    int getRoleIdByUsername(@Param("username")String username);
+    int getRoleIdByUsername(@Param("username") String username);
+
+    @Update("update user set nick_name = #{nick_name}, email = #{email}, phone = #{phone}, sex=#{sex} where username = #{username}")
+    void changeInformationByUsername(User user);
+
+    @Update("update user set name = #{name}, id_card = #{id_card}, image = #{image} where username = #{username}")
+    void authentication(User user);
+
 }
